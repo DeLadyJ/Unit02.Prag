cc65988f2ee656092db4a0769e981df058e18dd9
Ja_Lew <dladyj@gmail.com>
2019-05-26 15:24:43 -0400

    Changed Pagination Code and Deleted extra comments

--- js/script.js
+++ js/script.js
@@ -7,16 +7,6 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/ 
-/*** 
  * Create 2 global variables 
  * - 1 stores the list elements
  * - 2 stores the number of units viewed on each page
@@ -24,25 +14,12 @@ FSJS project 2 - List Filter and Pagination
 */
 const namesList = document.querySelectorAll('.student-item');
 const pageView = 10;
-const page = document.querySelector('.page')
+let pagediv = document.querySelector('.page');
 
 console.log(namesList);
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 /***
  * Create showPage function
  * - hides the list
@@ -56,57 +33,52 @@ const showPage = (list, page) =>  {
    const end = page * pageView;
    for (let i = 0; i < list.length; i++) { 
       if (i => begin && i <= end) {
-         list[i].style.display = '';
+         list[i].style.display = "";
        } else {
-      list[i].style.display = 'none';
+      list[i].style.display = "none";
    
     }
   }
 }
 
-showPage(namesList, 1);
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+//showPage(namesList, 1);
 
 /** 
  * Create appendPageLinks functios
  * - this makes the buttons work with pagination
 */
-const appendPageLinks = (list) =>{
-   let numOfPages = list.length/ pageView;
-    numOfPages = math.ceil( pageView); 
-  let containorLinks = document.createElement ('div');
-    containorLinks.className = 'paginate';
-    containorLinks.appendChild(containorLinks);
-   containorLinks.appendChild(ul); 
-   let ul = document.getElementById ('ul');
-
+function appendPageLinks (list) {
+    numOfPages = Math.ceil(list.length/ pageView);
+    let paginateDiv = document.createElement("div");
+    pagediv.appendChild(paginateDiv);
+     paginateDiv.className = "paginate";
+    let paginateUl = document.createElement("ul");
+     paginateDiv.appendChild(paginateUl);
+   
    /**
     * Creates the elements li and a
     * adds the href to the page
     */
     
     for (i = 0; i <= numOfPages; i++) {
-        let li = document.createElement ('li'); 
-        let a = ocument.createElement('a');
-        a.href = '#';
-        a.textContent = i = 1;
-         ul.appendChild(li); 
-         a.appendChild(li);
+        let li = document.createElement ("li"); 
+        let a = ocument.createElement("a");
+        a.href = "#";
+        a.textContent = i + 1;
+        li.appendChild(a);
+        paginateUl.appendChild(li);
 
          if (i===0) {
             a.classList = 'active';
          }
-
+      }
     /**
      *Adds the event listener 
      */
-         a.addEventListener('click'), event => {
+         a.addEventListener('click', event => {
              numOfPages = event.target.innerText;
-            if (event.target.className = 'a')
-let pageNumber = document.querySelectorAll('a'); 
+            if ((event.target.className = 'a'));{
+              let pageNumber = document.querySelectorAll('a'); 
 
 /**
  * Removes the active page from the pagination link
@@ -118,27 +90,12 @@ for (let i = 0; i< pageNumber.length; i += 1) {
 /**
  * Adds the active page to the pagination link once the event listener is actived
  */
-a.className = event.target.textContext;
-event.target.classList.add('active');      
-    };
+   a.className = event.target.textContext;
+   event.target.classList.add('active');    
 
-   }
+
+    }
+    
+   });
+   showPage(namesList, 1);
    
-appendPageLinks(namesList) ;  
-
-// For (let index = buttons.length - 1; index > 1; ==index) {
- //Let button = buttons{index};
- //Button.addElementListener(‘click’, pagination.click);
-
-
- 
-// var ulist = document.getElementById ('ul');
- //var list = document.createElement ('li');
- //var anchor = document.createElement ('a');
- //anchor.setAttribute ('href', '#');
- //anchor.innerHTML = i;
- //list.appendChild('anchor');
- //ulist.appendChild (list);
-//}
- 
-
